437d1d0188f915390e170bbb16cb1949b6853b98
Zachary Geier <zachary.d.geier@accenturefederal.com>
2019-06-14 11:17:32 -0400

    added testing

---
+++ app/src/test/java/com/zdgeier/ourdiary/MainActivityTest.kt
@@ -0,0 +1,11 @@
+package com.zdgeier.ourdiary
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class MainActivityTest {
+    @Test
+    fun f() {
+        assertEquals("test", "test")
+    }
+}
